054ee9d93431a71ed9e225032f9883ef5dd10ad5
Darshan Singh <minymidas@gmail.com>
2024-04-13 22:18:27 +0100

    added metamask stuff

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from web3 import Web3
+w3 = Web3(Web3.HTTPProvider('https://rpc-evm-sidechain.xrpl.org'))  # replace with your sidechain's RP
 
 from Components.mod1 import get_account, get_account_info
 import requests
@@ -15,16 +17,18 @@ def get_standby_account():
     new_wallet = get_account(seed)
     return jsonify(classic_address=new_wallet.classic_address, seed=new_wallet.seed)
 
-@app.route('/get_standby_info', methods=['GET'])
-def get_standby_info_fixed():
-    standby_seed = 'rPNQ1n1oTFGjQxveCgE7j9guAAeQDyeozU'
-    accountInfo = get_account_info(standby_seed)
-    return jsonify(accountInfo)
+@app.route('/get_standby_info')
+def get_standby_info():
+    standby_address = '0x4b86Dcd16171FbD4a5e3F55f335E6D75b7EaAf5A'  # replace with your standby address
+    balance = w3.eth.get_balance(standby_address)
+    balance_in_ether = Web3.from_wei(balance, 'ether')
+    return jsonify({'address': standby_address, 'balance': balance_in_ether})
 
-@app.route('/get_seed_info/<standby_seed>', methods=['GET'])
-def get_standby_info(standby_seed):
-    accountInfo = get_account_info(standby_seed)
-    return jsonify(accountInfo)
+
+# @app.route('/get_seed_info/<standby_seed>', methods=['GET'])
+# def get_standby_info(standby_seed):
+#     accountInfo = get_account_info(standby_seed)
+#     return jsonify(accountInfo)
 
 # @app.route('/send_xrp/<seed>/<amount>/<destination>', methods=['POST'])
 # def send_xrp(seed, amount, destination):
